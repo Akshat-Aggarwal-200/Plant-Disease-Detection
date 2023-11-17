c7b1cf26b07f8288470b9a2c076454e65ce3b347
Akshat-Aggarwal-200 <akshataggarwal6004@gmail.com>
2023-11-17 14:31:46 +0530

    Model Updated

--- src/model_training/imageNet_model.py
+++ src/model_training/imageNet_model.py
@@ -1,8 +1,8 @@
-# src/models/train_mobilenet.py
+# src/models/train_inception.py
 
 import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications import MobileNet
+from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
 from tensorflow.keras.optimizers import Adam
@@ -13,14 +13,14 @@ if __name__ == "__main__":
     output_directory = "model_saved"
     no_of_clases = len(os.listdir(os.path.join(input_directory, 'train')))
 
-    # Load and train MobileNet
-    mobilenet = MobileNet(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
-    mobilenet_model = Sequential([
-        mobilenet,
+    # Load and train InceptionV3 (ImageNet)
+    inception = InceptionV3(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
+    inception_model = Sequential([
+        inception,
         GlobalAveragePooling2D(),
         Dense(128, activation='relu'),
-        Dense(len(os.listdir(os.path.join(input_directory, 'train'))), activation='softmax')
+        Dense(no_of_clases, activation='softmax')
     ])
-    mobilenet_train_generator = load_data(input_directory, input_size=(256, 256), subset='training')
-    mobilenet_validation_generator = load_data(input_directory, input_size=(256, 256), subset='validation')
-    train_model(mobilenet_model, mobilenet_train_generator, mobilenet_validation_generator, os.path.join(output_directory, 'mobilenet_model.h5'))
+    inception_train_generator = load_data(input_directory, input_size=(256, 256), subset='training')
+    inception_validation_generator = load_data(input_directory, input_size=(256, 256), subset='validation')
+    train_model(inception_model, inception_train_generator, inception_validation_generator, os.path.join(output_directory, 'inception_model.h5'))
